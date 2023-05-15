4449ec431a88624ae1481c1e4b70c91d5fd49533
Nivaldo Carvalho <nivaldo_j@outlook.com>
2023-05-15 14:40:52 -0300

    removed qualis spider as it is too error-prone

--- metaevents/spiders/qualis/selectors.py
+++
@@ -1,8 +0,0 @@
-EVENT_CLASSIFICATION_OPTIONS_SELECTOR = \
-    'select#form\:evento > option::attr("value")'
-
-CLASSIFICATION_RESULT_LINKS_SELECTOR = \
-    'div.resultados > table > tbody > tr > td > a'
-
-RESULT_LINK_ONCLICK_ARGS_SELECTOR = \
-    'a::attr("onclick")'
