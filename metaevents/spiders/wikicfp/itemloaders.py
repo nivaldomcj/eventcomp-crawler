539f61d3690492c901e46c540265ef7edfa13b2e
Nivaldo Carvalho <nivaldo_j@outlook.com>
2023-05-28 11:24:31 -0300

    fix items of wikicfp spider

--- metaevents/spiders/wikicfp/itemloaders.py
+++ metaevents/spiders/wikicfp/itemloaders.py
@@ -8,17 +8,15 @@ from metaevents.functions import html_to_text
 class WikiCfpEventItemLoader(ItemLoader):
     id_out = TakeFirst()
 
-    title_in = TakeFirst()
-    title_out = Compose(lambda x: x.strip() if isinstance(x, str) else None)
+    title_out = TakeFirst()
 
-    type_in = TakeFirst()
-    type_out = Compose(lambda x: x.strip() if isinstance(x, str) else None)
+    type_out = TakeFirst()
 
-    start_at_in = TakeFirst()
-    start_at_out = Compose(lambda x: parse(x) if isinstance(x, str) else None)
+    start_at_in = MapCompose(lambda x: parse(x) if isinstance(x, str) else None)
+    start_at_out = TakeFirst()
 
-    finish_at_in = TakeFirst()
-    finish_at_out = Compose(lambda x: parse(x) if isinstance(x, str) else None)
+    finish_at_in = MapCompose(lambda x: parse(x) if isinstance(x, str) else None)
+    finish_at_out = TakeFirst()
 
     location_out = TakeFirst()
 
